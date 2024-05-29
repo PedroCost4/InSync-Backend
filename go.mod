947f3715929b32f514472728e749f2c1ceb14e9d
PedroCost4 <pedro.costacdc@gmail.com>
2024-05-28 22:16:15 -0300

    feat: register user

--- go.mod
+++ go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.23.0
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
@@ -15,11 +18,8 @@ require (
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
-	google.golang.org/grpc v1.64.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
 )
--- handler/user.go
+++ handler/user.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/PedroCost4/movies-backend/pb"
 	"github.com/PedroCost4/movies-backend/schema"
+	"github.com/PedroCost4/movies-backend/utils"
+	"github.com/google/uuid"
 )
 
 type UserServer struct {
 	pb.UnimplementedUsersServer
 }
 
-func (s *UserServer) GetUser(ctx context.Context, userReq *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+func (s *UserServer) GetUser(ctx context.Context, userReq *pb.GetUserRequest) (*pb.UserResponse, error) {
 	user := schema.User{}
 	err := db.Get(&user, "SELECT * FROM users WHERE id = $1", userReq.Id)
 	if err != nil {
@@ -20,13 +22,45 @@ func (s *UserServer) GetUser(ctx context.Context, userReq *pb.GetUserRequest) (*
 	}
 
 	returnedUser := &pb.User{
-		Id:    user.ID.String(),
-		Name:  user.Username,
-		Email: user.Email,
+		Id:       user.ID.String(),
+		UserName: user.Username,
+		Email:    user.Email,
 	}
 
-	return &pb.GetUserResponse{
+	return &pb.UserResponse{
 		User: returnedUser,
 	}, nil
 
+}
+
+func (s *UserServer) Register(ctx context.Context, userReq *pb.CreateUserRequest) (*pb.UserResponse, error) {
+	hashedPassword, err := utils.HashPassword(userReq.Password)
+
+	if err != nil {
+		logger.Errorf("Unable to hash password: %v\n", err)
+		return nil, err
+	}
+
+	newUUID := uuid.New()
+
+	id := uuid.UUID{}
+	err = db.QueryRowx(`INSERT INTO
+		users (id, username, email, password)
+	    VALUES ($1, $2, $3, $4)
+		RETURNING id`, newUUID.String(), userReq.UserName, userReq.Email, hashedPassword).Scan(&id)
+
+	if err != nil {
+		logger.Errorf("Unable to create user: %v\n", err)
+		return nil, err
+	}
+
+	returnedUser := &pb.User{
+		Id:       id.String(),
+		UserName: userReq.UserName,
+		Email:    userReq.Email,
+	}
+
+	return &pb.UserResponse{
+		User: returnedUser,
+	}, nil
 }
